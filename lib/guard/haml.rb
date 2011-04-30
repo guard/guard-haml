b68da8a87eb65e5c1f938a6342372ff503e77630
Immanuel Häussermann <haeussermann@gmail.com>
2011-04-30 18:00:07 +0200

    fix engine call and compile output message

--- lib/guard/haml.rb
+++ lib/guard/haml.rb
@@ -5,7 +5,7 @@ require 'haml'
 module Guard
   class Haml < Guard
     
-    VERSION = '0.0.1'
+    VERSION = '0.1.0'
     
     def initialize(watchers = [], options = {})
       @watchers, @options = watchers, options
@@ -13,26 +13,24 @@ module Guard
     
     def compile_haml file
       content = File.new(file).read
-      engine = Haml::Engine.new(content)
+      engine = ::Haml::Engine.new(content)
       engine.render
     end
     
     def run_all
       patterns = @watchers.map { |w| w.pattern }
       files = Dir.glob('**/*.*')
-      paths = files.select do |file|
-        patterns.select do |pattern| 
-          file if file.match(Regexp.new(pattern))
-        end
+      paths = files.map do |file|
+        patterns.map  { |pattern| file if file.match(Regexp.new(pattern)) }
       end
-      run_on_change(paths.flatten)
+      run_on_change(paths.flatten.compact)
     end
   
     def run_on_change(paths)
       paths.each do |file|
-        output_file = file[-1, -4]
+        output_file = file.split('.')[0..-2].join('.')
         File.open(output_file, 'w') { |f| f.write(compile_haml(file)) }
-        puts "# compiled '#{file}' to haml at '#{output_file}'"
+        puts "# compiled haml in '#{file}' to html in '#{output_file}'"
       end
     end
   end
