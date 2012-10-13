5b8ba099001227baad3a29d960d43ae242bb4317
Oleg Utkin <utkin.oleg@me.com>
2012-10-13 16:15:43 -0700

    Update lib/guard/haml.rb
    Removed hashtag... from message... very important... >_>

--- lib/guard/haml.rb
+++ lib/guard/haml.rb
@@ -64,7 +64,7 @@ module Guard
         engine  = ::Haml::Engine.new(content, (@options[:haml_options] || {}))
         engine.render
       rescue StandardError => error
-        message = " # HAML compilation failed!\nError: #{error.message}"
+        message = "HAML compilation failed!\nError: #{error.message}"
         ::Guard::UI.error message
         Notifier.notify( false, message ) if @options[:notifications]
         raise message
